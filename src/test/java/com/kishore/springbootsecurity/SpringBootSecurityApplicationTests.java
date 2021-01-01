a1cddce9d81528f8cfdfb8e3f817280e6bd4e39c
kishore <kishore@mallow-tech.com>
2021-01-01 10:28:02 +0530

    latest commit

--- src/test/java/com/kishore/springbootsecurity/SpringBootSecurityApplicationTests.java
+++ src/test/java/com/kishore/springbootsecurity/SpringBootSecurityApplicationTests.java
@@ -1,11 +1,13 @@
 package com.kishore.springbootsecurity;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
 @SpringBootTest
-class SpringBootSecurityApplicationTests {
+class SpringElasticBeanstalkApplicationTests {
 
     @Test
     void contextLoads() {
